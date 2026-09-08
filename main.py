043885ae8629908ec437f781f2d14258070578b9
YannickJong <yannickjong@hotmail.nl>
2026-09-08 22:42:44 +0200

    Got initial things working

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-def main():
-    print("Hello from hall-scheduler!")
+import pandas as pd
 
 
-if __name__ == "__main__":
-    main()
+def cleanup(input_file: str):
+    # Load the files/dataframes
+    hall_space: pd.DataFrame = pd.read_excel(input_file, sheet_name=0).set_index("Day")
+    time_slots: pd.DataFrame = pd.read_excel(input_file, sheet_name=1).set_index("Slot")
+    teams: pd.DataFrame = pd.read_excel(input_file, sheet_name=2).set_index("Name")
+    users: pd.DataFrame = pd.read_excel(input_file, sheet_name=3).set_index("Name")
+
+    # Check if hall space is used efficiently
+    total_hall_space: int = hall_space.sum().sum()
+    total_n_practices: float = teams["N practices"].sum()
+    if total_hall_space != total_n_practices:
+        if total_hall_space > total_n_practices:
+            print(f"WARNING: Inefficient scheduling, more hall space ({total_hall_space}) than practices ({total_n_practices})")
+        else:
+            raise Exception(f"More practices ({total_n_practices}) than hall space ({total_hall_space})!")
+
+    # Check the rotational teams
+    rotational_teams: pd.DataFrame = teams[teams["N practices"] * 2 % 2 == 1]
+    rotational_teams_count: pd.Series = rotational_teams.groupby("N practices")["N practices"].count()
+    num_rotational_slots: int = int(rotational_teams_count.sum() / 2)
+    print(num_rotational_slots)
+
+
+
+filename = "input_files/input.xlsx"
+cleanup(filename)
